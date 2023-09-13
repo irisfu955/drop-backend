f0cf4150cde64e0669fe65b2d4a0a8e299f5f092
irisfu955 <irisfu955@gmail.com>
2023-09-13 15:07:47 -0400

    verify token service

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+// const routes = require('./routes');
+ const { errorConverter } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
+
+const app = express();
+
+// parse json request body
+app.use(express.json({ limit: '10mb' }));
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+// app.use(errorHandler);
+
+module.exports = app;
